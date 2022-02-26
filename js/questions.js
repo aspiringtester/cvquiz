b9d71c780e9ea415b76f102ad709c5e7071a005f
paramita-Bhattacharya <paramitabhattacharya09@gmail.com>
2022-02-27 07:13:26 +1100

    committed

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which is the third planet from the sun?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which is the third planet from the sun?",
+    answer: "Sun",
+    options: [
+      "Saturn",
+      "Earth",
+      "Pluto",
+      "Mars"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is the largest ocean on Earth?",
+    answer: "Arctic Ocean",
+    options: [
+      "Atlantic Ocean",
+      "Indian Ocean",
+      "Arctic Ocean",
+      "Pacific Ocean"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the capital of Australia",
+    answer: "Canberra",
+    options: [
+      "Sydney",
+      "Canberra",
+      "Melbourne",
+      "Perth"
+    ]
+  },
+];
